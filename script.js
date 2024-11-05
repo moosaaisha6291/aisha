1eb16b3b9b0a29cd8fdc2a19967d8d65d528f201
CPS <98105494+cyberphantomsyndicate@users.noreply.github.com>
2024-11-05 12:07:07 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const music = document.getElementById('bg-music');
+    const music = document.getElementById('Tum_Hi_Ho_-_Aashiqui_2_128_Kbps.mp3');
     let isPlaying = true;
 
     document.body.addEventListener('click', function() {
